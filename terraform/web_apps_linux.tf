8a116787b672ff4dcd9664589e17eb023d29b77a
Benjamin Chiverton <b.chiverton@warwick.ac.uk>
2023-02-06 14:37:07 +0000

    Add jaeger UI

--- terraform/web_apps_linux.tf
+++ terraform/web_apps_linux.tf
@@ -1,34 +1,31 @@
-# resource "azurerm_app_service_plan" "linux" {
-#   name                = "${var.name}-plan-linux"
-#   location            = azurerm_resource_group.default.location
-#   resource_group_name = azurerm_resource_group.default.name
-#   kind                = "Linux"
+resource "azurerm_service_plan" "linux" {
+  name                = "${var.name}-plan-linux"
+  resource_group_name = azurerm_resource_group.default.name
+  location            = azurerm_resource_group.default.location
+  os_type             = "Linux"
+  sku_name            = var.plan_sku
+}
 
-#   reserved = true
+resource "azurerm_linux_web_app" "jaeger" {
+  name                = "${var.dns_prefix}-${var.name}-${var.environment}-jaeger"
+  resource_group_name = azurerm_resource_group.default.name
+  location            = azurerm_resource_group.default.location
+  service_plan_id     = azurerm_service_plan.linux.id
 
-#   sku {
-#     tier = var.plan_tier
-#     size = var.plan_sku
-#   }
-# }
+  identity {
+    type = "SystemAssigned"
+  }
 
-# resource "azurerm_app_service" "jaeger" {
-#   name                = "${var.dns_prefix}-${var.name}-${var.environment}-jaeger"
-#   location            = azurerm_resource_group.default.location
-#   resource_group_name = azurerm_resource_group.default.name
-#   app_service_plan_id = azurerm_app_service_plan.linux.id
+  site_config {
+    always_on = false // free tier
+    application_stack = {
+      docker_image     = "jaegertracing/all-in-one"
+      docker_image_tag = "1.42"
+    }
+  }
 
-#   site_config {
-#     always_on        = false // free tier
-#     linux_fx_version = "DOCKER|jaegertracing/all-in-one:1.42"
-#   }
-
-#   identity {
-#     type = "SystemAssigned"
-#   }
-
-#   app_settings = {
-#     "COLLECTOR_OTLP_ENABLED" = "true",
-#     "COLLECTOR_ZIPKIN_HOST_PORT" = ":9411"
-#   }
-# }
+  app_settings = {
+    "COLLECTOR_OTLP_ENABLED"     = "true",
+    "COLLECTOR_ZIPKIN_HOST_PORT" = ":9411"
+  }
+}
