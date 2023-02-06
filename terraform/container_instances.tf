bf210c781642aacb216c17d9eba46508c9bb8876
Benjamin Chiverton <b.chiverton@warwick.ac.uk>
2023-02-06 19:43:16 +0000

    Update container_instances.tf

--- terraform/container_instances.tf
+++ terraform/container_instances.tf
@@ -7,7 +7,7 @@ resource "azurerm_container_group" "jaeger" {
 
   container {
     name   = "hello-world"
-    image  = "registry.hub.docker.com/library/jaegertracing/all-in-one:1.42"
+    image  = "docker.io/jaegertracing/all-in-one:1.42"
     cpu    = "0.5"
     memory = "1.5"
 
