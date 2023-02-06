7b313d4f80e3d039065d801515e62f80c1e174da
Benjamin Chiverton <b.chiverton@warwick.ac.uk>
2023-02-06 16:42:46 +0000

    Update web_apps_linux.tf

--- terraform/web_apps_linux.tf
+++ terraform/web_apps_linux.tf
@@ -6,26 +6,26 @@ resource "azurerm_service_plan" "linux" {
   sku_name            = var.plan_sku
 }
 
-# resource "azurerm_linux_web_app" "jaeger" {
-#   name                = "${var.dns_prefix}-${var.name}-${var.environment}-jaeger"
-#   resource_group_name = azurerm_resource_group.default.name
-#   location            = azurerm_resource_group.default.location
-#   service_plan_id     = azurerm_service_plan.linux.id
+resource "azurerm_linux_web_app" "jaeger" {
+  name                = "${var.dns_prefix}-${var.name}-${var.environment}-jaeger"
+  resource_group_name = azurerm_resource_group.default.name
+  location            = azurerm_resource_group.default.location
+  service_plan_id     = azurerm_service_plan.linux.id
 
-#   identity {
-#     type = "SystemAssigned"
-#   }
+  identity {
+    type = "SystemAssigned"
+  }
 
-#   site_config {
-#     always_on = false // free tier
-#     application_stack {
-#       docker_image     = "jaegertracing/all-in-one"
-#       docker_image_tag = "1.42"
-#     }
-#   }
+  site_config {
+    always_on = false // free tier
+    # application_stack {
+    #   docker_image     = "jaegertracing/all-in-one"
+    #   docker_image_tag = "1.42"
+    # }
+  }
 
-#   app_settings = {
-#     "COLLECTOR_OTLP_ENABLED"     = "true",
-#     "COLLECTOR_ZIPKIN_HOST_PORT" = ":9411"
-#   }
-# }
+  app_settings = {
+    "COLLECTOR_OTLP_ENABLED"     = "true",
+    "COLLECTOR_ZIPKIN_HOST_PORT" = ":9411"
+  }
+}
