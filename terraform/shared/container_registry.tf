af26a552d02973b3a08ea8e2fc953a0d12c4ec5a
Benjamin Chiverton <b.chiverton@warwick.ac.uk>
2023-02-08 21:06:13 +0000

    Update container_registry.tf

--- terraform/shared/container_registry.tf
+++ terraform/shared/container_registry.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "acr" {
-  name                = "${var.name}containerregistry" 
+  name                = "${var.name}containerregistry"
   resource_group_name = azurerm_resource_group.shared.name
   location            = azurerm_resource_group.shared.location
   sku                 = "Basic"
