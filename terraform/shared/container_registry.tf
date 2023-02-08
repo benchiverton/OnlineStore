ff7a4702abf9459700a98a45a228040df9b797a5
Benjamin Chiverton <b.chiverton@warwick.ac.uk>
2023-02-08 20:29:42 +0000

    Add admin for ACR (#38)

--- terraform/shared/container_registry.tf
+++ terraform/shared/container_registry.tf
@@ -1,6 +1,7 @@
 resource "azurerm_container_registry" "acr" {
-  name                = "${var.name}containerregistry" 
+  name                = "${var.name}containerregistry"
   resource_group_name = azurerm_resource_group.shared.name
   location            = azurerm_resource_group.shared.location
   sku                 = "Basic"
+  admin_enabled       = true
 }
