62f0d389a40f2c43b95b7e0fc9e81257d17361fe
Benjamin Chiverton <b.chiverton@warwick.ac.uk>
2023-02-10 12:46:51 +0000

    Add opentelemetry integration

--- src/Api/Company.Api/Program.cs
+++ src/Api/Company.Api/Program.cs
@@ -1,8 +1,17 @@
+using System;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,6 +28,26 @@ builder.Services.AddCors(policy => policy.AddPolicy("CorsPolicy", opt => opt
         .AllowAnyMethod()));
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "Online Store API", Version = "v1" }));
+
+var serviceName = Assembly.GetExecutingAssembly().GetName().Name.ToString();
+var serviceVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+var appResourceBuilder = ResourceBuilder.CreateDefault()
+    .AddService(serviceName: serviceName, serviceVersion: serviceVersion);
+var meter = new Meter(serviceName);
+builder.Services.AddOpenTelemetry()
+    .WithTracing(tracerProviderBuilder => tracerProviderBuilder
+        .AddOtlpExporter(opt => opt.Endpoint = new Uri(builder.Configuration.GetValue<string>("OTLPExporter:Endpoint")))
+        .AddSource(serviceName)
+        .SetResourceBuilder(
+            ResourceBuilder.CreateDefault()
+                .AddService(serviceName: serviceName, serviceVersion: serviceVersion))
+        .AddAspNetCoreInstrumentation())
+    .WithMetrics(metricProviderBuilder => metricProviderBuilder
+        .AddOtlpExporter(opt => opt.Endpoint = new Uri(builder.Configuration.GetValue<string>("OTLPExporter:Endpoint")))
+        .AddMeter(meter.Name)
+        .SetResourceBuilder(appResourceBuilder)
+        .AddAspNetCoreInstrumentation())
+    .StartWithHost();
 
 var host = builder.Build();
 
