1a2f621674045a7d3f77c9c4a02629204cd5d438
Benjamin Chiverton <b.chiverton@warwick.ac.uk>
2023-02-12 13:41:53 +0000

    dns name label needs to be unique

--- terraform/instance/container_instances.tf
+++ terraform/instance/container_instances.tf
@@ -3,7 +3,7 @@ resource "azurerm_container_group" "jaeger" {
   resource_group_name = azurerm_resource_group.instance.name
   location            = azurerm_resource_group.instance.location
   ip_address_type     = "Public"
-  dns_name_label      = "${var.name}-jaeger"
+  dns_name_label      = "${var.name}-${lower(var.environment)}-jaeger"
   os_type             = "Linux"
 
   container {
