de2ae72280333275ae91239b37d48eaad6f70dc9
benc <benjichiverton@gmail.com>
2024-06-24 16:11:35 +0100

    Update container_apps.tf

--- terraform/instance/container_apps.tf
+++ terraform/instance/container_apps.tf
@@ -5,7 +5,7 @@ resource  "azurerm_container_app_environment" "apps" {
 }
 
 resource "azurerm_container_app" "api" {
-  name                         = "api"
+  name                         = "${var.name}-api"
   container_app_environment_id = azurerm_container_app_environment.apps.id
   resource_group_name          = azurerm_resource_group.instance.name
   revision_mode                = "Single"
@@ -16,6 +16,14 @@ resource "azurerm_container_app" "api" {
       image  = "mcr.microsoft.com/azuredocs/containerapps-helloworld:latest"
       cpu    = 0.25
       memory = "0.5Gi"
+    }
+  }
+
+  ingress {
+    external_enabled = true
+    target_port      = 80
+    traffic_weight {
+      percentage = 100
     }
   }
 }
