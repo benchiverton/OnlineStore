1f7427a6140b6f956b09f29e65cf6cdd6b3e18fe
benc <benjichiverton@gmail.com>
2024-06-25 15:26:32 +0100

    Update overwrite-config.sh

--- src/Website/Company.Website/overwrite-config.sh
+++ src/Website/Company.Website/overwrite-config.sh
@@ -1,1 +1,3 @@
+echo "running overwrite-config!"
+cat /var/www/web/appsettings.json
 echo "`jq --arg APIBASEPATH "$API__BASEPATH" '.Api.BasePath=$APIBASEPATH' /var/www/web/appsettings.json`" > /var/www/web/appsettings.json
