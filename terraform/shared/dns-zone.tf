4e1c46accdab1e796f6d36c04aa816307ac13043
Benjamin Chiverton <b.chiverton@warwick.ac.uk>
2024-07-01 08:31:18 +0100

    Update dns-zone.tf

--- terraform/shared/dns-zone.tf
+++ terraform/shared/dns-zone.tf
@@ -1,4 +1,4 @@
-resource "azurerm_dns_zone" "englishwinetourism-co-uk" {
-  name                = "englishwinetourism.co.uk"
+resource "azurerm_dns_zone" "rockpal-co-uk" {
+  name                = "rockpal.co.uk"
   resource_group_name = azurerm_resource_group.shared.name
 }
