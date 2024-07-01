28f4b13d3164e2a800e66ab5d5206bd1d9f98f21
Benjamin Chiverton <b.chiverton@warwick.ac.uk>
2024-07-01 18:42:28 +0100

    Tidy up terraform + workflows

--- terraform/instance/dns.tf
+++ terraform/instance/dns.tf
@@ -21,20 +21,6 @@ resource "azurerm_dns_txt_record" "api" {
   }
 }
 
-# resource "azurerm_container_app_custom_domain" "api" {
-#   name             = trimsuffix(azurerm_dns_cname_record.api.fqdn, ".")
-#   container_app_id = azurerm_container_app.api.id
-
-#   depends_on = [
-#     azurerm_dns_txt_record.api,
-#   ]
-
-#   lifecycle {
-#     // When using an Azure created Managed Certificate these values must be added to ignore_changes to prevent resource recreation.
-#     ignore_changes = [certificate_binding_type, container_app_environment_certificate_id]
-#   }
-# }
-
 resource "azurerm_dns_cname_record" "website" {
   name                = var.website_dns_subdomain
   zone_name           = data.azurerm_dns_zone.rockpal-co-uk.name
@@ -53,20 +39,6 @@ resource "azurerm_dns_txt_record" "website" {
   }
 }
 
-# resource "azurerm_container_app_custom_domain" "website" {
-#   name             = trimsuffix(azurerm_dns_cname_record.website.fqdn, ".")
-#   container_app_id = azurerm_container_app.website.id
-
-#   depends_on = [
-#     azurerm_dns_txt_record.website,
-#   ]
-
-#   lifecycle {
-#     // When using an Azure created Managed Certificate these values must be added to ignore_changes to prevent resource recreation.
-#     ignore_changes = [certificate_binding_type, container_app_environment_certificate_id]
-#   }
-# }
-
 resource "azurerm_dns_cname_record" "monitoring" {
   name                = var.monitoring_dns_subdomain
   zone_name           = data.azurerm_dns_zone.rockpal-co-uk.name
@@ -84,20 +56,6 @@ resource "azurerm_dns_txt_record" "monitoring" {
     value = azurerm_container_app.monitoring.custom_domain_verification_id
   }
 }
-
-# resource "azurerm_container_app_custom_domain" "monitoring" {
-#   name             = trimsuffix(azurerm_dns_cname_record.monitoring.fqdn, ".")
-#   container_app_id = azurerm_container_app.monitoring.id
-
-#   depends_on = [
-#     azurerm_dns_txt_record.monitoring,
-#   ]
-
-#   lifecycle {
-#     // When using an Azure created Managed Certificate these values must be added to ignore_changes to prevent resource recreation.
-#     ignore_changes = [certificate_binding_type, container_app_environment_certificate_id]
-#   }
-# }
 
 module "container_apps_bind_dns" {
   source                                = "./container_apps_bind_dns"
