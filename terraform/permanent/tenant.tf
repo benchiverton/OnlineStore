41706c689ab8266cd9058cf78ee721d2358311ec
Benjamin Chiverton <b.chiverton@warwick.ac.uk>
2024-07-03 09:25:17 +0100

    Update tenant.tf

--- terraform/permanent/tenant.tf
+++ terraform/permanent/tenant.tf
@@ -1,6 +1,6 @@
 resource "azurerm_aadb2c_directory" "rockpal" {
   country_code            = "UK"
-  data_residency_location = "United Kingdom"
+  data_residency_location = "Europe"
   display_name            = "rockpal-b2c-tenant"
   domain_name             = "rockpal.co.uk"
   resource_group_name     = azurerm_resource_group.permanent.name
