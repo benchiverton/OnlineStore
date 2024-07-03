435791c56fb6bd09d6fcb93f8db48972aeb556eb
Benjamin Chiverton <b.chiverton@warwick.ac.uk>
2024-07-03 09:22:59 +0100

    Create rockpal tenant

---
+++ terraform/permanent/tenant.tf
@@ -0,0 +1,8 @@
+resource "azurerm_aadb2c_directory" "rockpal" {
+  country_code            = "UK"
+  data_residency_location = "United Kingdom"
+  display_name            = "rockpal-b2c-tenant"
+  domain_name             = "rockpal.co.uk"
+  resource_group_name     = azurerm_resource_group.permanent.name
+  sku_name                = "PremiumP1"
+}
