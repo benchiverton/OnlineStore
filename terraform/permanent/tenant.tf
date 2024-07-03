264aeb06079053b6ebfbb0e5e0af734d37288e6b
Benjamin Chiverton <b.chiverton@warwick.ac.uk>
2024-07-03 09:40:53 +0100

    Delete tenant.tf

--- terraform/permanent/tenant.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_aadb2c_directory" "rockpal" {
-  country_code            = "GB"
-  data_residency_location = "Europe"
-  display_name            = "rockpal-b2c-tenant"
-  domain_name             = "rockpal.onmicrosoft.com"
-  resource_group_name     = azurerm_resource_group.permanent.name
-  sku_name                = "PremiumP1"
-}
