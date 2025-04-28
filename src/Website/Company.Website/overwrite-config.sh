101a7230073eb158a14498ee60f179c801732a29
Benjamin Chiverton <benjichiverton@gmail.com>
2025-04-28 21:07:04 +0100

    Typo

--- src/Website/Company.Website/overwrite-config.sh
+++ src/Website/Company.Website/overwrite-config.sh
@@ -4,4 +4,4 @@ echo "`jq --arg APIBASEPATH "$API__BASEPATH" '.Api.BasePath=$APIBASEPATH' /var/w
 echo "`jq --arg APICLIENTID "$API__CLIENTID" '.Api.ClientId=$APICLIENTID' /var/www/web/appsettings.json`" > /var/www/web/appsettings.json
 echo "`jq --arg WEBSITECLIENTID "$AZUREADB2C__CLIENTID" '.AzureAdB2C.ClientId=$WEBSITECLIENTID' /var/www/web/appsettings.json`" > /var/www/web/appsettings.json
 echo "`jq --arg USEFAKEAUTH "$USEFAKEAUTH" '.UseFakeAuth=$USEFAKEAUTH' /var/www/web/appsettings.json`" > /var/www/web/appsettings.json
-echo "`jq --arg CHATSERVERADDRESS "CHATSERVERADDRESS" '.ChatServer.Address=CHATSERVERADDRESS' /var/www/web/appsettings.json`" > /var/www/web/appsettings.json
+echo "`jq --arg CHATSERVERADDRESS "$CHATSERVER__ADDRESS" '.ChatServer.Address=CHATSERVERADDRESS' /var/www/web/appsettings.json`" > /var/www/web/appsettings.json
